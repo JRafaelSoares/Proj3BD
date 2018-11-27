1ed6b81f2a6dfde07ba1ead5bc7dd4c97f2ec91d
Pedro Rodrigues <pedroprodrigues98@gmail.com>
2018-11-27 13:28:02 +0000

    Improved interface design

--- web/gerir.php
+++ web/gerir.php
@@ -1,58 +1,60 @@
 <html>
     <body>
-<?php
-    
-    $type = $_REQUEST['type'];
-    try
-    {
-        $host = "db.ist.utl.pt";
-        $user ="ist187666";
-        $password = "joana0101";
-        $dbname = $user;
-        
-        $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        $sql = "SELECT * FROM " . $type . ";"; // 
+        <?php
+            
+            $type = $_REQUEST['type'];
+            $column_names = eval("return " . $_REQUEST['columnNames'] . ";");
 
-        $result = $db->prepare($sql);
-        $result->execute();
+            try
+            {
+                $host = "db.ist.utl.pt";
+                $user ="ist187666";
+                $password = "joana0101";
+                $dbname = $user;
+                
+                $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                $sql = "SELECT * FROM " . $type . ";"; // 
 
-        echo("<table border=\"1\">\n");
-        
-        $result = $result->fetchAll();
+                $result = $db->prepare($sql);
+                $result->execute();
 
-        $column_names = array_keys($result[0]);
-        $array_size = count($column_names);
-        //Nomes das colunas
-        echo("<tr>");
-        $i = 0;
-        while($i < $array_size){
-            echo("<td>");
-            echo($column_names[$i]);
-            echo("</td>");
-            $i = $i + 2;
-        }
-        echo("</tr>");
-        //Objetos
-        foreach ($result as $row) {
-            echo("<tr>");
-            $i = 0;
-            while($i < $array_size/2){
-                echo("<td>");
-                echo($row[$i]);
-                echo("</td>");
-                $i = $i + 1;
+                $array_size = count($column_names);
+
+                echo("<table border=\"1\">\n");
+
+                //Nomes das colunas
+                echo("<tr>");
+                foreach ($column_names as $column_name){
+                    echo("<td>");
+                    echo($column_name);
+                    echo("</td>");
+                }
+                echo("</tr>");
+
+                //Objetos
+                foreach ($result as $row) {
+                    echo("<tr>");
+
+                    $i = 0;
+
+                    while($i < $array_size){
+                        echo("<td>");
+                        echo($row[$i]);
+                        echo("</td>");
+                        $i = $i + 1;
+                    }
+
+                    echo("</tr>");
+                }
+                echo("</table>\n");
+                $db = null;
             }
-            echo("</tr>");
-        }
-        echo("</table>\n");
-        $db = null;
-    }
-    catch (PDOException $e)
-    {
-        echo("<p>ERROR: {$e->getMessage()}</p>");
-    }
-?>
+            catch (PDOException $e)
+            {
+                echo("<p>ERROR: {$e->getMessage()}</p>");
+            }
+        ?>
     </body>
 </html>
